aa60c5f0dd98b9bcddf48e2fca6d691547e6f3a1
DreamMln <maix2374@edu.zealand.dk>
2022-04-08 12:20:04 +0200

    test get

--- PairProgrammingMLTest/UnitTest1.cs
+++ PairProgrammingMLTest/UnitTest1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
@@ -18,31 +17,32 @@ namespace PairProgrammingMLTest
     public class UnitTest1
     {
         //installing driver to c
-        private static readonly string _chromeDriver = "C:\\WebDriver";
-
-        private static readonly string _foxDriver = "C:\\Drivers";
-        private static readonly string _edgeDriver = "C:\\webDrivers";
-       
+        private static readonly string DriverDirectory = "C:\\WebDriver";
         private static IWebDriver _driver;
 
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
-            _driver = new ChromeDriver(_chromeDriver); // fast
-            _driver = new FirefoxDriver(_foxDriver);
-            _driver = new EdgeDriver(_edgeDriver);
-        }
-
-        [ClassCleanup]
-        //static behover ikke har return typen hvor void er empty
-        public static void TearDown()
-        {
-            _driver.Dispose();
+            _driver = new ChromeDriver(DriverDirectory); // fast
         }
 
         [TestMethod]
         public void TestMethod1()
         {
+            _driver.Navigate().GoToUrl("file:///C:/JS/ParProgrammingML/ParProgrammingML/index.html");
+
+            string title = _driver.Title;
+            Assert.AreEqual("Par programming", title);
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            IWebElement duathletesList = wait.Until(d => d.FindElement(By.Id("getDuathlete")));
+
+            //Assert.AreEqual("Bibibib", duathletesList.Text);
+            //Console.WriteLine(duathletesList.Text);
+
+            Assert.IsTrue(duathletesList.Text.Contains("Eddy5"));
+
+
 
         }
     }
