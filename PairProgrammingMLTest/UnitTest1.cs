3a8ef32ae2eefe52d846fea478e7409bcea123e2
Lava Maamo <lama26830@edu.ucl.dk>
2022-04-08 10:22:34 +0200

    drivers

--- PairProgrammingMLTest/UnitTest1.cs
+++ PairProgrammingMLTest/UnitTest1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
@@ -17,18 +18,32 @@ namespace PairProgrammingMLTest
     public class UnitTest1
     {
         //installing driver to c
-        private static readonly string DriverDirectory = "C:\\WebDriver";
+        private static readonly string _chromeDriver = "C:\\WebDriver";
+
+        private static readonly string _foxDriver = "C:\\Drivers";
+        private static readonly string _edgeDriver = "C:\\webDrivers";
+       
         private static IWebDriver _driver;
 
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
-            _driver = new ChromeDriver(DriverDirectory); // fast
+            _driver = new ChromeDriver(_chromeDriver); // fast
+            _driver = new FirefoxDriver(_foxDriver);
+            _driver = new EdgeDriver(_edgeDriver);
+        }
+
+        [ClassCleanup]
+        //static behover ikke har return typen hvor void er empty
+        public static void TearDown()
+        {
+            _driver.Dispose();
         }
 
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
